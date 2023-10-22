9033616fb67062f29a05fee491bec7dc5a0887ad
IamErol <yildirimerol7.0.0@gmail.com>
2023-10-23 03:39:57 +0500

    latest

--- main_app/routers/user.py
+++ main_app/routers/user.py
@@ -1,8 +1,9 @@
+from typing import List
 from fastapi import APIRouter, HTTPException, status, Response
 import logging
 from main_app.oauth import authenticate_user, create_access_token
 from main_app import schemas, crud
-from typing import List
+
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
@@ -42,6 +43,7 @@ async def get_all_users(skip: int = 0, limit: int = 100):
 
 @router.put("/user", status_code=status.HTTP_200_OK, response_model=schemas.User)
 async def update_user_by_id(id, user: schemas.UserBase):
+    """Update user by id."""
     logger.debug("Updating user...", extra={"id": id})
     user = await crud.update_user(id, user)
     return user
@@ -49,14 +51,16 @@ async def update_user_by_id(id, user: schemas.UserBase):
 
 @router.delete("/user")
 async def delete_user_by_id(id):
+    """Delete user."""
     logger.debug("Deleting user from db", extra={"id": id})
     # query = users.update().where(users.c.id == user.id).values(name=user.name, email=user.email)
-    crud.delete_user(id)
+    await crud.delete_user(id)
     return Response(status_code=status.HTTP_200_OK)
 
 
 @router.post("/token", response_model=schemas.Token)
 async def login(user: schemas.UserCreate):
+    """Login user."""
     logger.debug("Getting access token...")
     user = await authenticate_user(user.email, user.password)
     access_token = create_access_token(user.email)
@@ -66,7 +70,7 @@ async def login(user: schemas.UserCreate):
 @router.get("/users/name", status_code=status.HTTP_200_OK, response_model=List[schemas.User])
 async def get_user_by_name(name: str):
     """Search users by name."""
-    logger.debug("Search user by name...", extra={"id": id})
+    logger.debug("Searching user by name...", extra={"id": id})
     result = await crud.search_by_name(name=name)
     if result:
         return result
@@ -74,11 +78,11 @@ async def get_user_by_name(name: str):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not found.")
 
 
-@router.get("/users/filter", status_code=status.HTTP_200_OK, response_model=List[schemas.User])
-async def filtering_and_sorting(filter: str, sort_by: str):
-    """Filtering and sorting."""
-    result = await crud.filter_and_sort_users(filter=filter, sort_by=sort_by)
-    if result:
-        return result
-    else:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not found.")
+# @router.get("/users/filter", status_code=status.HTTP_200_OK, response_model=List[schemas.User])
+# async def filtering_and_sorting(filter: str, sort_by: str):
+#     """Filtering and sorting."""
+#     result = await crud.filter_and_sort_users(filter=filter, sort_by=sort_by)
+#     if result:
+#         return result
+#     else:
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not found.")
